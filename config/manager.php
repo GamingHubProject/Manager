0371f4ab0b1ab9b2343dd793b714e119fea5d7dd
Rose <jrose96e@gmail.com>
2026-08-06 12:27:53 +0200

    Update manager.php

--- config/manager.php
+++ config/manager.php
@@ -3,7 +3,7 @@
 return [
     'official_registry_url' => env(
         'GAMING_HUB_MANAGER_OFFICIAL_REGISTRY_URL',
-        'https://raw.githubusercontent.com/RosesOfDorns/gaming-hub-registry/main/registry.json'
+        'https://raw.githubusercontent.com/GamingHubProject/Registry/main/registry.json'
     ),
     'official_registry_fallback' => plugin_path('gaming-hub-manager/resources/registry/official.json'),
     'allow_private_hosts' => (bool) env('GAMING_HUB_MANAGER_ALLOW_PRIVATE_HOSTS', false),
